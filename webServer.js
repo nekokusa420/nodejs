8634fd09cbc8081d51036df950d27d4c3005770b
nekokusa420 <sk011010ab@gmail.com>
2024-02-24 06:56:15 +0800

    98

--- webServer.js
+++ webServer.js
@@ -1,8 +1,35 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log("Request has received");
+  // console.log("Request has received");
+  // console.log(req.url);
+  // console.log(req.method);
+
+  let path = "./page/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+  res.setHeader("Content-Type", "text/html");
+  fs.readFile(path, (error, data) => {
+    if (error) console.log(error);
+    else res.write(data);
+    res.end();
+  });
 });
+
+// res.write('<meta charset="utf-8" />');
+// res.write("<h1>hello</h1>");
+// res.write("<h2>NodeJS入門課程</h2>");
+// res.end();
 
 server.listen(3000, "localhost", () => {
   console.log("server is listening port 3000");
